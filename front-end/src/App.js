6eb2f91b07f402da6bf71251b73b42277b2a992d
Karthikeya <bairojukarthikeya@gmail.com>
2024-04-22 21:44:44 +0530

    update app.ja

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,8 @@ import './App.css';
 function App() {
   return(
     <div class="container">
-      <h1>hello, world</h1>
-      <h2>hello, world</h2>
-      <h3>hello, world</h3>
-      <h4>hello, world</h4>
-      <h5>hello, world</h5>
-      <h6>hello, world</h6>
+      <h1>Hello, world </h1>
     </div>
-
   )
 }
 
